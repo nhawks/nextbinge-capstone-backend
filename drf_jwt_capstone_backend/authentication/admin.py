9c1983902639a6865ef4e67cf6758f80987d4a39
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-11-03 17:35:20 -0400

    Added WatchList and WatchedShows.

--- drf_jwt_capstone_backend/authentication/admin.py
+++ drf_jwt_capstone_backend/authentication/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import User
+from .models import WatchList
+from .models import WatchedShows
 
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(WatchedShows)
+admin.site.register(WatchList)
