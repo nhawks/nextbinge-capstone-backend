b43fc23665951e32992bb3c37690d8110fcc6ac3
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-11-03 17:28:08 -0400

    Initial creation of  Watched and WatchList models.

--- drf_jwt_capstone_backend/authentication/models.py
+++ drf_jwt_capstone_backend/authentication/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.fields import CharField
 from django.db.models.fields.json import JSONField
 
 # Create your models here.
@@ -14,3 +15,13 @@ from django.db.models.fields.json import JSONField
 class User(AbstractUser):
     streaming_providers = JSONField(default=dict)
 
+
+class WatchedShows(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tv_show = models.CharField(max_length=75)
+    is_favorite = models.BooleanField("Favorite Show", default=False)
+    user_rating = models.BooleanField("User Rating", default=False)
+
+
+class WatchList(models.Model):
+    tv_show = models.CharField(max_length=75)
