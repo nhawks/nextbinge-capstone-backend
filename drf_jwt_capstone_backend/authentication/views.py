9265476c6be7f7151f85fa9155e65d98fd537fcf
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-11-04 18:31:26 -0400

    Added endpoint to get the user's information.

--- drf_jwt_capstone_backend/authentication/views.py
+++ drf_jwt_capstone_backend/authentication/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth import get_user_model
+from rest_framework import serializers, status
+from rest_framework.response import Response
+from rest_framework.decorators import permission_classes
+from rest_framework.views import APIView
 from .serializers import RegistrationSerializer
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 User = get_user_model()
 
 
@@ -9,3 +13,18 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegistrationSerializer
+
+
+class UserDetail(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise status.HTTP_404_NOT_FOUND
+
+    def get(self, request, pk):
+        user = self.get_object(pk)
+        serializer = RegistrationSerializer(user)
+        return Response(serializer.data, status=status.HTTP_302_FOUND)
