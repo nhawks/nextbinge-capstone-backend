c8096e7bb09798f1f7fb6c2016d05a1a8812feb3
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-11-04 02:38:36 -0400

    Updated urls for updating favorites.
    - Added paths for adding, removing, and removing all favorites.

--- drf_jwt_capstone_backend/shows/urls.py
+++ drf_jwt_capstone_backend/shows/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from . import views
 
+
+app_name = "shows"
 urlpatterns = [
     #? localhost/api/show/...path
     #* GET: user's watched shows | POST: mark show as watched/favorite
     path('watched/', views.watched_shows),
 
     #* GET: user's favorite shows
-    path('favorites/', views.favorite_shows),
+    path('favorites/', views.favorite_shows, name="favorites"),
+    path('favorites/add', views.update_favorites),
+    path('favorites/remove', views.update_favorites),
+    path('favorites/remove-all', views.update_favorites),
 ]
