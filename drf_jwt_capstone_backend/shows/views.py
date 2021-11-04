9de9a014d2719576715bbe4cf81bf468b76114a9
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-11-03 22:45:22 -0400

    Users can now remove shows from favorites.

--- drf_jwt_capstone_backend/shows/views.py
+++ drf_jwt_capstone_backend/shows/views.py
@@ -34,15 +34,19 @@ def watched_shows(request):
     else:
         return Response(status.HTTP_405_METHOD_NOT_ALLOWED)
 
-@api_view(["GET"])
+@api_view(["GET", "PATCH"])
 @permission_classes([AllowAny])
 def favorite_shows(request):
+    if request.method != "GET" and request.method != "PATCH":
+        return Response(status.HTTP_405_METHOD_NOT_ALLOWED)
+    shows = WatchedShows.objects.filter(Q(user_id=request.user.id) & Q(is_favorite=True))
     if request.method == "GET":
-        shows = WatchedShows.objects.filter(Q(user_id=request.user.id) & Q(is_favorite=True))
         serializer = WatchedShowsSerializer(shows, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    else:
-        return Response(status.HTTP_405_METHOD_NOT_ALLOWED)
+    if request.method == "PATCH":
+        show = shows.filter(pk=request.data["id"])
+        show = show.update(is_favorite=F('is_favorite') - 1)
+        return Response(status=status.HTTP_200_OK)
 
 # ? WatchList Views
 
